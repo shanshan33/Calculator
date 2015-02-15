6730c3467bd49b9fa812c1caae0536f87870f89c
Shanshan ZHAO <chezzhaoss@gmail.com>
2015-02-15 19:21:46 +0100

    Create digit pad for the calculator

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Shanshan ZHAO on 15/02/15.
+//  Copyright (c) 2015 Shanshan ZHAO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var isInTheMiddleOfTypingANumber : Bool = false
+    
+    @IBAction func PressDigit(sender: UIButton)
+    {
+        let digit = sender.currentTitle!
+        if(isInTheMiddleOfTypingANumber)
+        {
+        displayLabel.text = displayLabel.text! + digit
+        } else
+        {
+            displayLabel.text = digit
+            isInTheMiddleOfTypingANumber = true
+        }
+    }
+    var stack = Array<Double>()
+
+    @IBAction func enter()
+    {
+        isInTheMiddleOfTypingANumber = false
+        stack.append(displayDoubleValue)
+        println("stack =\(stack)")
+    }
+    
+    // To fix the problem string-> double, "!" will crash when it's nil
+        var displayDoubleValue : Double
+        {
+        get{
+            return NSNumberFormatter().numberFromString(displayLabel.text!)!.doubleValue
+        }
+        set{
+            displayLabel.text = "\(newValue)"
+            isInTheMiddleOfTypingANumber = false
+        }
+    }
+
+
+}
+
